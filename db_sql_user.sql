61cc46b05ef3673bcc1781887927352daf494516
csnam_home <csnam515@gmail.com>
2019-08-28 19:15:33 +0900

    user sql 문 추가

---
+++ db_sql_user.sql
@@ -0,0 +1,8 @@
+insert into tbl_user values (1001,"csnam515@gmail.com","홍길동","1234","서울 강남구 강남대로 1", 10000,"qrdata1");
+insert into tbl_user values (1002,"yyyy@gmail.com","강길동","1234","서울 강남구 강남대로 2", 20000,"qrdata1");
+insert into tbl_user values (1003,"XXXX@gmail.com","김길동","1234","서울 강남구 강남대로 31", 30000,"qrdata1");
+insert into tbl_user values (1004,"AAAAA@gmail.com","나길동","1234","서울 강남구 강남대로 32", 40000,"qrdata1");
+insert into tbl_user values (1005,"BBBBB@gmail.com","너길동","1234","서울 강남구 강남대로 33", 10000,"qrdata1");
+insert into tbl_user values (1006,"CCCCC@gmail.com","성길동","1234","서울 강남구 강남대로 44", 20000,"qrdata1");
+insert into tbl_user values (1007,"DDDDD@gmail.com","차길동","1234","서울 강남구 강남대로 55", 30000,"qrdata1");
+insert into tbl_user values (1008,"EEEEE@gmail.com","이길동","1234","서울 강남구 강남대로 66", 40000,"qrdata1");
